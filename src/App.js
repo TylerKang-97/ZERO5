4de0fde1c7b323cf814df614b71730c56fcce44f
youngil Ko <koyoungil97@gmail.com>
2021-11-24 19:50:23 +0900

    server.js 작업중 (gitignore 명시)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import './App.css';
-import Home from './Home';
-import About from './About';
+import Home from '.client/Home';
+import About from './client/About';
 
 function App() {
-  
-  return (
-    <div>
-      <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/about" element={ <About /> } />
-        </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/about' element={<About />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
