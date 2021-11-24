3c020330a6235a58d3851796e99ed0f5b7f0bdbe
tylerkang <kingksy777@gmail.com>
2021-11-24 16:47:48 +0900

    Router 설계 진행중_1124

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React , { useEffect, useState} from "react";
+// import { makeStyles } from '@material-ui/core/styles';
+
+const Home = () => {
+
+    return(
+        <div>
+            <h1>Home Pages</h1>
+        </div>
+    );
+
+
+}
+
+export default Home;
+
