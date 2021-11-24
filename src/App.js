d7aacbca1b34d8b701e1f75bcbdb8cde890c0d9f
tylerkang <kingksy777@gmail.com>
2021-11-24 16:46:51 +0900

    Router 설계 진행중_1124

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { Route, Routes, Router, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import About from './About';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hello!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+      <Route exact path = "/" component={Home} />
+      <Route path="/about" component={About} />
+      </Routes>
     </div>
   );
 }
