612e9837d961fc8ad6ff1db6332b8f62edc3a898
tylerkang <kingksy777@gmail.com>
2021-11-30 15:44:07 +0900

    About.js_Carousel적용_1130

--- src/client/About.js
+++ src/client/About.js
@@ -1,12 +1,21 @@
 import React , { useEffect, useState} from "react";
 import { useLocation, Link } from "react-router-dom";
 import AboutInfo from "../info/AboutInfo";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import '../css/About.css'
 
 const About = () => {
     const { pathname } = useLocation();
     console.log(pathname);
-
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
     return(
         <>
         <div className="about_background">
@@ -14,6 +23,37 @@ const About = () => {
             <h1 className="about_h1">About</h1>
             <p>ZERO 5는 부착형 스마트 카트모듈을 제작중인 창업팀입니다.</p>
             </Link>
+            <Slider {...settings}>
+                
+                    <div className="about_carousel">
+                    <h3 className="about_carousel_h1">S.S.A.C</h3>
+                    <p className="about_carousel_p">부착형 스마트 카트 모듈</p>
+                    </div>
+                    
+                    <div className="about_carousel">
+                    <h3 className="about_carousel_h1">Box Farmers</h3>
+                    <p className="about_carousel_p">교육용 자동 식물 재배 키트</p>
+                    </div>
+
+                    <div className="about_carousel">
+                    <h3 className="about_carousel_h1">명글 카메라</h3>
+                    <p className="about_carousel_p">OCR(광학 문자 인식) 기법을 이용한 안드로이드 APP</p>
+                    </div>
+
+                    <div className="about_carousel">
+                    <h3 className="about_carousel_h1">Movie Salon</h3>
+                    <p className="about_carousel_p">영화 정보 웹페이지</p>
+                    </div>
+
+                    <div className="about_carousel">
+                    <h3>5</h3>
+                    </div>
+
+                    <div className="about_carousel">
+                    <h3>6</h3>
+                    </div>
+                
+      </Slider>
         </div>
         {pathname === '/about' ? <AboutInfo /> : null }
         {/*
--- src/info/AboutInfo.js
+++ src/info/AboutInfo.js
@@ -7,8 +7,9 @@ const AboutInfo = () => {
             <h1 className="about_h1">Detail</h1>
             <div className="about_container">
                 <div className="about_left_side">
-                    <h1>똑똑한 서비스를 쉽고 간편하게 제공하다!</h1>
-                    <h1>다양한 IT기술들을 기존에 것들에 부착하여 새로운 서비스를!</h1>
+                    <h1>ZERO 5는 다양한 IT 기술들을 기존의 것들에 부착하여 새로운 서비스를
+                        제공하는것을 목표로 하고 있습니다. </h1>
+                    
                 </div>
                 <div className="about_right_side">
                     <h2>2021.04 팀개설</h2>
