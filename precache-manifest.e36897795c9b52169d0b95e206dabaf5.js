336141c6ba89a71471f3d4c5daf1542af9f36b45
tylerkang <kingksy777@gmail.com>
2021-12-02 14:26:03 +0900

    Updates

---
+++ precache-manifest.e36897795c9b52169d0b95e206dabaf5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024505f7e0b35603c6e3d2fa34b11280",
+    "url": "http://TylerKang-97.github.io/ZERO5/index.html"
+  },
+  {
+    "revision": "14903df36aa64036be90",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/css/2.bf3a3c8d.chunk.css"
+  },
+  {
+    "revision": "25b8d68bb99f25d3c687",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/css/main.411fdef1.chunk.css"
+  },
+  {
+    "revision": "14903df36aa64036be90",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/js/2.b0cc48de.chunk.js"
+  },
+  {
+    "revision": "3b84315683ff5403a050ec2732a63886",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/js/2.b0cc48de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b8d68bb99f25d3c687",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/js/main.98c8cb89.chunk.js"
+  },
+  {
+    "revision": "48295f1e09e80f5b75f4",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/js/runtime-main.2687a050.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "http://TylerKang-97.github.io/ZERO5/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ZERO5/precache-manifest.f86ac93a6ceb7383bc0fc536073a8374.js"
+  "http://TylerKang-97.github.io/ZERO5/precache-manifest.e36897795c9b52169d0b95e206dabaf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ZERO5/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://TylerKang-97.github.io/ZERO5/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
